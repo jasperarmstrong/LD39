7d1f75ccb2123f5c1e22f41d4f46f44dc33331af
Jasper Armstrong <jasper.a.1996@gmail.com>
2017-07-29 23:48:46 -0400

    added the EMPController class, currently only registers an OnDeath

---
+++ Assets/Scripts/EMPController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMPController : MonoBehaviour {
+	Health health;
+
+	void Start () {
+		health = GetComponent<Health>();
+		health.OnDeath += () => {
+			Debug.Log("emp destroyed!");
+			Destroy(gameObject);
+		};
+	}
+	
+	void Update () {
+		
+	}
+}
